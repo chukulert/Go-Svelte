7611f3d0ea8bf30d266487d2084e2ebc55123680
Daniel Tan <dantwq90@gmail.com>
2022-11-24 08:38:38 +0800

    Update NotFound.svelte

--- frontend/src/page/NotFound.svelte
+++ frontend/src/page/NotFound.svelte
@@ -1,9 +1,22 @@
+
 <script>
     import {navigate} from "svelte-routing";
     import Button from "../UI/Button.svelte";
+    import { onMount } from "svelte";
+
+    let check = false;
+    onMount(()=>{
+        if(sessionStorage.getItem("JWT")){
+            check = true;
+        }
+    })
 </script>
 
 <main class="page-container">
     <h2>Page not found</h2>
-    <Button on:click={() => navigate("/dashboard")}>Return Home</Button>
+    {#if check}
+    <Button on:click={() =>   window.location.replace("/dashboard")}>Return Dashboard</Button>
+    {:else}
+    <Button on:click={() => window.location.replace("/")}>Return LoginPage</Button>
+    {/if}
 </main>
