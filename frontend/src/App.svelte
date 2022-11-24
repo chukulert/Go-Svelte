7d0f3508eb9038a713cf36a891b27cfb7b5c9c91
Daniel Tan <dantwq90@gmail.com>
2022-11-24 08:32:58 +0800

    Update App.svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,27 +1,65 @@
 <script>
-  import { Router, Route } from "svelte-routing";
+  import { Router } from "svelte-routing";
   import ProtectedRoutes from "./utils/ProtectedRoutes.svelte";
   import Dashboard from "./page/Dashboard.svelte";
-  import Homepage from "./page/Homepage.svelte";
   import UserManagement from "./page/UserManagement.svelte";
   import Profile from "./page/Profile.svelte";
   import GroupManagement from "./page/GroupManagement.svelte";
   import NotFound from "./page/NotFound.svelte";
   import AppForm from "./page/AppForm.svelte";
-  // import TaskForma from "./page/TaskForma.svelte";
-    import TaskForm from "./page/TaskForma.svelte";
+  import TaskForm from "./page/TaskForma.svelte";
+  import { onMount } from "svelte"
+
   // Used for SSR. A falsy value is ignored by the Router.
   export let url = "";
+  let isadmin = false; 
+  let loaded = false;
+  let nonadmin = true;
+
+  onMount(() =>{
+    let token = sessionStorage.getItem("JWT");
+    if (token != undefined || token != null) {
+      const url = "http://localhost:8080/authorize";
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          token: token,
+          group: "admin",
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.Success) {
+            isadmin = data.Success;
+            loaded = true;
+          } else {
+            nonadmin = !data.Success;
+            loaded = true;
+          }
+        })
+        .catch((error) => {
+          loaded = true;
+        });
+    } else {
+      loaded = true; 
+    }
+  });
 </script>
 
 <Router {url}>
-  <Route path="/" component={Homepage} />
-
+  {#if loaded && (nonadmin || isadmin)}
   <ProtectedRoutes path="/dashboard" component={Dashboard} />
   <ProtectedRoutes path="/appForm" component={AppForm} />
   <ProtectedRoutes path="/taskForm" component={TaskForm} />
   <ProtectedRoutes path="/profile" component={Profile} />
+  {:else if loaded}
+  <ProtectedRoutes component={NotFound} />
+  {/if}
+
+  {#if isadmin && loaded}
   <ProtectedRoutes path="/userManagement" component={UserManagement}/>
   <ProtectedRoutes path="/groupManagement" component={GroupManagement}/>
-  <Route component={NotFound} />
+  {:else if loaded}
+  <ProtectedRoutes component={NotFound} />
+  {/if}
 </Router>
