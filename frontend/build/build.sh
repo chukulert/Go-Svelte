f1a3502688c5ada526d7e70d59f3253471cc263e
Daniel Tan <dantwq90@gmail.com>
2022-11-24 08:39:33 +0800

    Update build.sh

--- frontend/build/build.sh
+++ frontend/build/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-cd "C:/Users/commonuser/Desktop/buildspace/Ff-pHdWJ/0/gitlab-instance-1f5f55d9/"
+cd "C:/Users/commonuser/Desktop/buildspace/Ff-pHdWJ/0/gitlab-instance-1f5f55d9/frontend/"
 
-tar -cvzf frontend.gz .\frontend\
+npm install
 
-ssh commonuser@192.168.0.3
-"scp 192.168.0.2:C:/Users/commonuser/Desktop/bin_repo/frontend.gz 192.168.0.3:/C:/Users/commonuser/Desktop/deployed"
+npm run build
+
+ls
+
+tar -czf frontend.gz ./config ./public ./deploy ./package.json ./server.js ./node_modules
+
+mv C:/Users/commonuser/Desktop/buildspace/Ff-pHdWJ/0/gitlab-instance-1f5f55d9/frontend/frontend.gz C:/Users/commonuser/Desktop/bin_repo
+
